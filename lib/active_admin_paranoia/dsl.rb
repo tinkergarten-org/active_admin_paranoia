25f83e4d74ec071702016df06d261db40742a69f
Ariel Juodziukynas <arieljuod@gmail.com>
2023-11-26 14:42:59 -0300

    DMM-21 Use kargs splat operator for the args instea of the merge

--- lib/active_admin_paranoia/dsl.rb
+++ lib/active_admin_paranoia/dsl.rb
@@ -9,7 +9,7 @@ module ActiveAdminParanoia
         options = { notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_archived', count: ids.count, model: resource_class.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
         # For more info, see here: https://github.com/rails/rails/pull/22506
         if Rails::VERSION::MAJOR >= 5
-          controller.redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(**options))
+          controller.redirect_back(**{ fallback_location: ActiveAdmin.application.root_to }.merge(options))
         else
           controller.redirect_to :back, options
         end
@@ -40,7 +40,7 @@ module ActiveAdminParanoia
         options = { notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: ids.count, model: resource_class.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
         # For more info, see here: https://github.com/rails/rails/pull/22506
         if Rails::VERSION::MAJOR >= 5
-          redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(**options))
+          redirect_back(**{ fallback_location: ActiveAdmin.application.root_to }.merge(options))
         else
           redirect_to :back, options
         end
@@ -63,7 +63,7 @@ module ActiveAdminParanoia
         options = { notice: I18n.t('active_admin_paranoia.batch_actions.succesfully_restored', count: 1, model: resource_class.model_name, plural_model: resource_class.to_s.downcase.pluralize) }
         # For more info, see here: https://github.com/rails/rails/pull/22506
         if Rails::VERSION::MAJOR >= 5
-          redirect_back({ fallback_location: ActiveAdmin.application.root_to }.merge(**options))
+          redirect_back(**{ fallback_location: ActiveAdmin.application.root_to }.merge(options))
         else
           redirect_to :back, options
         end
